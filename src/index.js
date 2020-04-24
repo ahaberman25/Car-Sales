d3f1d33ab86e04772b5f0dbeedfbc5dca2d0749e
Andrew Haberman <a.haberman25@gmail.com>
2020-04-24 12:13:02 -0400

    redux set up and cleanup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import App from './App';
+import { rootReducer } from "./reducers/";
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore(rootReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+//import reducers
+
+export const rootReducer = combineReducers({
+  
+});
